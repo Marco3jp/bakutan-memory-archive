35016ea296c0ec006a8c0b3ede9e32c379074f21
marco <marco3jp+git@gmail.com>
2024-03-02 23:38:33 +0900

    クライアントサイドで動かせるように使うパッケージを見直し

--- frontend/app/components/TweetWrapper.tsx
+++ frontend/app/components/TweetWrapper.tsx
@@ -1,4 +1,7 @@
-import { Tweet } from 'react-tweet'
+// 公式ドキュメントの通りにimportすると index.client.js の Tweet コンポーネントを使ってしまう
+// これらは内部で getTweet, fetchTweet を呼び、Twitter syndication API を叩くのでCORSエラーで使えない
+// swr.js のほうに実装されている Tweet コンポーネントは useTweet を呼び、これは CORS friendly な vercel のAPIを使ってくれる
+import { Tweet } from 'node_modules/react-tweet/dist/swr.js'
 import "./tweet-wrapper.scss"
 
 export function TweetWrapper(props: {photoTweetId: string}) {
