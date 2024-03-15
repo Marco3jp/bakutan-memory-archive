1c7ba54e356413fb21c14592d0b0279c452c2540
Marco <28888207+Marco3jp@users.noreply.github.com>
2024-03-15 17:41:56 +0000

    並び順が現実順じゃないんだけどざっと後夜祭のツイートを集めた（しきい値はRT30）

--- frontend/app/assets/2024/bakutan/afterparty.ts
+++ frontend/app/assets/2024/bakutan/afterparty.ts
@@ -3,15 +3,184 @@ import {Stage} from "~/model/event";
 export const afterparty: Stage = {
     name: "Vの宴2024 feat.さなのばくたん。",
     locationName: "CLUB CITTA' + 月あかり夢てらす",
+    // チャプター未整理
     chapters: [
         {
-            name: "アフターパーティー",
+            name: "アフターパーティーその1",
+            ratio: 1,
+            memories: [
+               {
+                     tweetUrl: "https://twitter.com/Lain0r4/status/1766483877802180704"
+                },
+                {
+                     tweetUrl: "https://twitter.com/OJavanP/status/1766493595551588811"
+                },
+                {
+                     tweetUrl: "https://twitter.com/denonbu_world/status/1766500716527710360"
+                },
+                {
+                     tweetUrl: "https://twitter.com/Lain0r4/status/1766503055833313390"
+                },
+                {
+                     tweetUrl: "https://twitter.com/Lain0r4/status/1766503540061593672"
+                },
+                {
+                     tweetUrl: "https://twitter.com/OJavanP/status/1766504677204713892"
+                },
+                {
+                     tweetUrl: "https://twitter.com/Muda913/status/1766506004408090708"
+                },
+                {
+                     tweetUrl: "https://twitter.com/poi99poi/status/1766506645037765034"
+                },
+                {
+                     tweetUrl: "https://twitter.com/poi99poi/status/1766509763422982294"
+                },
+                {
+                     tweetUrl: "https://twitter.com/fooooooomin/status/1766509911326806408"
+               }
+            ]
+        },
+        {
+            name: "アフターパーティーその2",
             ratio: 1,
             memories: [
                 {
-                    tweetUrl: "https://twitter.com/"
+                    tweetUrl: "https://twitter.com/tn4mdg1/status/1766511588406612447"
+                },
+                {
+                    tweetUrl: "https://twitter.com/VaistarStudio/status/1766512589649973249"
+                },
+                {
+                    tweetUrl: "https://twitter.com/kiyokiyo_r1/status/1766513607536189715"
+                },
+                {
+                    tweetUrl: "https://twitter.com/Omiya_Nie/status/1766515172393234876"
+                },
+                {
+                    tweetUrl: "https://twitter.com/saltwater255/status/1766515346846970083"
+                },
+                {
+                    tweetUrl: "https://twitter.com/Lain0r4/status/1766518085354426852"
+                },
+                {
+                    tweetUrl: "https://twitter.com/egoshia54/status/1766521222429573421"
+                },
+                {
+                    tweetUrl: "https://twitter.com/OJavanP/status/1766521900472348713"
+                },
+                {
+                    tweetUrl: "https://twitter.com/Omiya_Nie/status/1766521924564222280"
+                },
+                {
+                    tweetUrl: "https://twitter.com/mashira_ness/status/1766530365005680688"
+                },
+            ]
+        },
+        {
+            name: "アフターパーティーその3",
+            ratio: 1,
+            memories: [
+                {
+                    tweetUrl: "https://twitter.com/OJavanP/status/1766530793315442819"
+                },
+                {
+                    tweetUrl: "https://twitter.com/Muda913/status/1766530999201186280"
+                },
+                {
+                    tweetUrl: "https://twitter.com/poi99poi/status/1766531160472170607"
+                },
+                {
+                    tweetUrl: "https://twitter.com/OJavanP/status/1766532311892558157"
+                },
+                {
+                    tweetUrl: "https://twitter.com/Lain0r4/status/1766532998533681280"
+                },
+                {
+                    tweetUrl: "https://twitter.com/tonogames1/status/1766533267686347184"
+                },
+                {
+                    tweetUrl: "https://twitter.com/obera_1313/status/1766535728874570214"
+                },
+                {
+                    tweetUrl: "https://twitter.com/tn4mdg1/status/1766539391760220203"
+                },
+                {
+                    tweetUrl: "https://twitter.com/OJavanP/status/1766540992520548528"
+                },
+                {
+                    tweetUrl: "https://twitter.com/f22sentouki/status/1766541277623931263"
+                },
+            ]
+        },
+        {
+            name: "アフターパーティーその4",
+            ratio: 1,
+            memories: [
+                {
+                    tweetUrl: "https://twitter.com/tn4mdg1/status/1766542930721099935"
+                },
+                {
+                    tweetUrl: "https://twitter.com/tn4mdg1/status/1766549479237271858"
+                },
+                {
+                    tweetUrl: "https://twitter.com/Lain0r4/status/1766553348646539293"
+                },
+                {
+                    tweetUrl: "https://twitter.com/tm825cs/status/1766555514891366718"
+                },
+                {
+                    tweetUrl: "https://twitter.com/kiyokiyo_r1/status/1766563229923185044"
+                },
+                {
+                    tweetUrl: "https://twitter.com/kurousa_itzr/status/1766569963492417760"
+                },
+                {
+                    tweetUrl: "https://twitter.com/onasu_143831/status/1766570613416825299"
+                },
+                {
+                    tweetUrl: "https://twitter.com/TngSkr_mhr04/status/1766577132895941004"
+                },
+                {
+                    tweetUrl: "https://twitter.com/tn4mdg1/status/1766583652996034992"
+                },
+                {
+                    tweetUrl: "https://twitter.com/tofu_2co/status/1766590281200333172"
+                },
+            ]
+        },
+        {
+            name: "アフターパーティーその5",
+            ratio: 1,
+            memories: [
+                {
+                    tweetUrl: "https://twitter.com/may_yon27/status/1766590684314669501"
+                },
+                {
+                    tweetUrl: "https://twitter.com/SonanLine/status/1766591241355985069"
+                },
+                {
+                    tweetUrl: "https://twitter.com/Chihan37/status/1766600295314780609"
+                },
+                {
+                    tweetUrl: "https://twitter.com/babble042/status/1766601184553451981"
+                },
+                {
+                    tweetUrl: "https://twitter.com/oisiiramune/status/1766614344396419456"
+                },
+                {
+                    tweetUrl: "https://twitter.com/urami_debaisu/status/1766618373704802592"
+                },
+                {
+                    tweetUrl: "https://twitter.com/___sai___/status/1766622494474293459"
+                },
+                {
+                    tweetUrl: "https://twitter.com/pnpr_gm/status/1766780111557480533"
+                },
+                {
+                    tweetUrl: "https://twitter.com/kiyokiyo_r1/status/1766826169444495512"
                 }
             ]
-        }
+        },
     ]
 }
