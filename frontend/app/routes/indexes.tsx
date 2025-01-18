5178f7f34ae3a27bbf1ae2eeb1255a232549f57e
Marco <28888207+Marco3jp@users.noreply.github.com>
2025-01-18 17:49:38 +0000

    リンク先を追加

---
+++ frontend/app/routes/indexes.tsx
@@ -0,0 +1,15 @@
+import { Link } from "@remix-run/react";
+
+export default function Indexes() {
+    return (
+        <main>
+            <article>
+                <div>
+                    <Link to="/">
+                        <p>さなのばくたん。2024</p>
+                    </Link>
+                </div>
+            </article>
+        </main>
+    )
+}
