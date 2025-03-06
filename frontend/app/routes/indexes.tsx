da86266513d5b54758ae43c7dfd5dbbfe5db7d69
Marco <k.kazuma+git@marco.plus>
2025-03-06 22:35:28 +0900

    indexesページのデザインを整えた

--- frontend/app/routes/indexes.tsx
+++ frontend/app/routes/indexes.tsx
@@ -1,15 +1,16 @@
 import { Link } from "@remix-run/react";
 
-export default function Indexes() {
-    return (
-        <main>
-            <article>
-                <div>
-                    <Link to="/">
-                        <p>さなのばくたん。2024</p>
-                    </Link>
-                </div>
-            </article>
-        </main>
-    )
+export default function IndexesPage() {
+  return (
+    <div className="text-neutral-50 bg-gray-950 h-dvh w-dvw border border-natori-accent-pink rounded-sm">
+      <ul className="flex items-center justify-center h-full">
+        <li className="border border-natori-accent-pink rounded-xl p-8">
+          <Link to="/" className="text-2xl font-bold text-center flex flex-col">
+            <span>さなのばくたん。(2024)</span>
+            <span>王国からの招待状</span>
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
