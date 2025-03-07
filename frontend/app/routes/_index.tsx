aadece55752b7ea933c026c4f3af8c677622dd6a
Marco <k.kazuma+git@marco.plus>
2025-03-07 18:00:05 +0900

    索引の当たり判定悪かったので修正

--- frontend/app/routes/_index.tsx
+++ frontend/app/routes/_index.tsx
@@ -12,14 +12,14 @@ export default function IndexesPage() {
   return (
     <div className="text-neutral-50 bg-gray-950 h-dvh w-dvw border border-natori-accent-pink rounded-sm">
       <ul className="flex flex-col items-center justify-center h-full space-y-12 px-4">
-        <li className="border border-natori-accent-pink rounded-xl p-8 min-w-1/3">
-          <Link to="/bakutan2025" className="text-2xl font-bold text-center flex flex-col">
+        <li className="border border-natori-accent-pink rounded-xl min-w-1/3">
+          <Link to="/bakutan2025" className="text-2xl font-bold text-center flex flex-col p-8 w-full">
             <span>さなのばくたん。(2025)</span>
             <span className="text-xl">をやろうと思ったら異世界に転生していた件について</span>
           </Link>
         </li>
-        <li className="border border-natori-accent-pink rounded-xl p-8 min-w-1/3">
-          <Link to="/bakutan2024" className="text-2xl font-bold text-center flex flex-col">
+        <li className="border border-natori-accent-pink rounded-xl min-w-1/3">
+          <Link to="/bakutan2024" className="text-2xl font-bold text-center flex flex-col p-8 w-full">
             <span>さなのばくたん。(2024)</span>
             <span>王国からの招待状</span>
           </Link>
